21eef499ea6c65f98bec381a0f992e85d36761e7
Jarad Olson <brotherdust@gmail.com>
2023-09-13 09:59:26 -0600

    Update flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A basic gomod2nix flake";
+  description = "Flake to build rtlamr";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
